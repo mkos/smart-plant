2c750492dc461ba647b667ba662d5785e5ff8118
mkos <kosinski.michal@gmail.com>
2016-11-02 02:51:27 +0100

    reading both light and moisture readings

--- pcf8591_test.py
+++ pcf8591_test.py
@@ -10,8 +10,14 @@ bus = SMBus(1)
 
 last_reading =-1
 ain = 0
-bus.write_byte(0x48,0)
+
+LIGHT = 0
+MOISTURE = 1
+AUTOINCREMENT = 4
+
+bus.write_byte(0x48,AUTOINCREMENT)
 while(0 == 0):
-	reading = bus.read_byte(0x48)
-	print('reading: {}'.format(reading))
+	light = bus.read_word_data(0x48, MOISTURE)
+	moisture = bus.read_word_data(0x48, LIGHT)
+	print('light: {}, moisture: {}'.format(light & 0xff , moisture & 0xff))
 	time.sleep(1)
