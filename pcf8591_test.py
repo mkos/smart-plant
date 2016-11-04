0a900e09429a7212ab0b03b13a2fa56ba8ca823a
mkos <kosinski.michal@gmail.com>
2016-11-04 01:09:06 +0100

    encapsulate code with class, add some formatting

--- pcf8591_test.py
+++ pcf8591_test.py
@@ -5,19 +5,37 @@ import time
 
 
 from smbus import SMBus
-
-bus = SMBus(1)
-
-last_reading =-1
-ain = 0
+from collections import OrderedDict
 
 LIGHT = 0
 MOISTURE = 1
-AUTOINCREMENT = 4
+DEVICE_ID = 0x48
 
-bus.write_byte(0x48,AUTOINCREMENT)
-while(0 == 0):
-	light = bus.read_word_data(0x48, MOISTURE)
-	moisture = bus.read_word_data(0x48, LIGHT)
-	print('light: {}, moisture: {}'.format(light & 0xff , moisture & 0xff))
+bus = SMBus(1)
+
+class PCF8591(object):
+
+    autoincrement = 0x04
+
+    def __init__(self, bus_handle, device_id):
+        self.bus = bus_handle
+        self.id = device_id
+
+    def start(self):
+        self.bus.write_byte(self.id, self.autoincrement)
+
+    def read(self):
+        result = []
+        for i in range(4):
+	    result.append(self.bus.read_word_data(self.id,i+1) & 0xff)
+
+        return result
+
+
+adc = PCF8591(bus, DEVICE_ID)
+adc.start()
+
+while(True):
+        readings = OrderedDict(zip(['light', 'moisture', 'unused_3', 'unused_4'], adc.read()))
+        print ', '.join(['{}: {}'.format(k,v) for k, v in readings.items() if not k.startswith('unused')])
 	time.sleep(1)
